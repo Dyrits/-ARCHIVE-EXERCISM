bd92b406c8c8c779d66b96ebbc044bc8c093f1db
dyrits <dyrits@outlook.fr>
2020-06-19 12:03:14 +0200

    New additions.

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,8 @@
 module ResistorColorDuo
-  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-  def self.value(colorPair)
-    "#{COLORS.index(colorPair[0])}#{COLORS.index(colorPair[1])}".to_i
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+  def self.value(color_pair)
+    color_pair.map {|element| COLORS.index(element)}.first(2).join.to_i
   end
 end
+
 
