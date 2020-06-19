a12c4b5e35e1c8fa42f53de2d22681885fb8b83a
dyrits <dyrits@outlook.fr>
2020-06-19 13:41:31 +0200

    Exercises added.

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,3 @@
 const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
-export const decodedValue = (colorPair) => Number([COLORS.indexOf(colorPair[0]), COLORS.indexOf(colorPair[1])].join(""));
+export const decodedValue = (colorPair) => Number(COLORS.indexOf(colorPair[0]) * 10 + COLORS.indexOf(colorPair[1]));
